243f0544cb0440176632b769006da1b3a41e2109
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-04-07 14:04:50 -0300

    commit2

--- Primos.py
+++ Primos.py
@@ -3,35 +3,38 @@ import unittest
 
 def is_primo(value):
     # %-->resto
-    div=2 
-    while value% div ==0:
+    if value <= 1:  # Si el número es 1 o menor, no es primo
         return False
-    return True
-
+    for i in range(2, value): #un rango desde el dos hasta el numero elegido
+        if value % i == 0:    #si el resto da 0 no es primo, por lo tanto devuelve Falso
+            return False
+    return True    #caso contrario devuelve Verdadero
+        
 
 class TestPrimos(unittest.TestCase):
     def test_1(self):
         result = is_primo(1)
-        self.assertEqual(result, True)
-
+        self.assertEqual(result, False)
+    
     def test_2(self):
         result = is_primo(2)
         self.assertEqual(result, True)
 
     def test_3(self):
         result = is_primo(3)
-        self.assertEqual(result, True)        
+        self.assertEqual(result, True)
 
     def test_4(self):
         result = is_primo(4)
-        self.assertEqual(result, False)  
+        self.assertEqual(result, False)
 
     def test_5(self):
         result = is_primo(5)
-        self.assertEqual(result, False)  
+        self.assertEqual(result, True)
 
     def test_6(self):
         result = is_primo(6)
-        self.assertEqual(result, False)  
+        self.assertEqual(result, False)
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
